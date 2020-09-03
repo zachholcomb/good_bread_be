5ed33b0d218cce80e16297ddd187b84ac0d2ca1a
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-03 13:29:58 -0600

    Adds initial controller for items

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::ItemsController < ApplicationController
+  def create
+    return render json: Error.missing_params, status: :bad_request if missing_params?
+
+    render json: ItemSerializer.new(Item.create(item_params)), status: :created
+  end
+
+  def index
+    render json: ItemSerializer.new(Item.all)
+  end
+
+  def show
+    return render json: Error.not_found, status: :not_found if item?
+
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def update
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    render json: ItemSerializer.new(item.delete)
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :price)
+  end
+
+  def missing_params?
+    params[:name].blank? || params[:price].blank?
+  end
+
+  def item?
+    Item.where(id: params[:id]).empty?
+  end
+end

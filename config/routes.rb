2f33ccac9bdfa339f254ebcb7e94d86e3e09a762
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-03 23:33:08 -0600

    Add notes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+
+      # USER SPECIFIC ROUTES
       resources :users, except: [:new] do
         resources :shipments, only: [:index, :show], controller: :users_shipments
         resources :subscription, only: [:create, :update, :destroy]
       end
+
+      # ADMIN ROUTES
       resources :subscriptions, only: [:index, :show]
       resources :items, except: [:new]
       resources :shipments, except: [:new]

8bb17a0d920e720107a339a1843b81bc60dc2ed3
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-03 23:30:59 -0600

    Adds separate route for shipments to a user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, except: [:new] do
+        resources :shipments, only: [:index, :show], controller: :users_shipments
         resources :subscription, only: [:create, :update, :destroy]
       end
       resources :subscriptions, only: [:index, :show]

f3db56b0bac057798862d501f8be5ef7213c4059
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-07 00:15:12 -0600

    Adds new action for sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,21 @@
 class Api::V1::SessionsController < ApplicationController
   before_action :authorize_access_request!, only: [:destroy]
+
+  def new
+    user = User.new(user_params)
+    if user.save
+      payload = { user_id: user.id }
+      session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
+      tokens = session.login
+      response.set_cookie(JWTSessions.access_cookie,
+                          value: tokens[:access],
+                          httponly: true,
+                          secure: Rails.env.production?)
+      render json: { csrf: tokens[:csrf] }, status: :created
+    else
+      render json: { error: user.errors.full_messages.join(' ') }, status: :unprocessable_entity
+    end
+  end
   
   def create
     user = User.find_by(email: params[:email])
@@ -24,4 +40,8 @@ class Api::V1::SessionsController < ApplicationController
   end
 
   private
+
+  def user_params
+    params.permit(:email, :name, :address, :password, :password_confirmation)
+  end
 end 

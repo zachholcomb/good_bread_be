52ed3aafe492cb1acb0770c5a09769059473f8d9
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-06 22:37:11 -0600

    Adds current user and unauthorized methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  include JWTSessions::RailsAuthorization
+  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
+  
+  def current_user
+    @current_user ||= User.find(payload['user_id'])
+  end
+
+  private
+
+  def not_authorized
+    render json: Error.unauthorized, status: :unauthorized
+  end
 end

12a36803bfd616f216d93a5b44c65d5086d9e3b3
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-06 22:36:59 -0600

    Adds test for user login/logout

---
+++ spec/requests/api/v1/sessions/user_login_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe 'login request' do
+  before(:each) do
+    @user = User.create!(
+        email: 'john@example.com',
+        name: 'John Wick',
+        address: '900 Victoria St.',
+        password: '1234',
+        password_confirmation: '1234'
+      )
+  end
+
+  it 'user can login' do
+    login_params = {
+      "email": "john@example.com",
+      "password": "1234"
+    }
+
+    post '/api/v1/login', params: login_params
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    login = JSON.parse(response.body, symbolize_names: true)
+    expect(login[:csrf]).to_not eq(nil)
+  end
+
+  it 'incorrect credentials are sent unauthorized message' do
+    login_params = {
+      "email": "john@example.com",
+      "password": "wrong_password"
+    }
+
+    post '/api/v1/login', params: login_params
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+
+    failed_login = JSON.parse(response.body, symbolize_names: true)
+    expect(failed_login[:data][:attributes][:message]).to eq("Email or password doesn't match")
+  end
+
+  it 'user can logout' do
+    login_params = {
+      "email": "john@example.com",
+      "password": "1234"
+    }
+
+    post '/api/v1/login', params: login_params
+    expect(response).to be_successful
+    credentials = JSON.parse(response.body, symbolize_names: true)
+    token = credentials[:csrf]
+
+    header = {
+      "X-CSRF-TOKEN": token
+    }
+
+    logout_params = {
+      "email": "john@example.com",
+      "password": "1234"
+    }
+
+    delete '/api/v1/logout', params: logout_params, headers: header
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    resp = JSON.parse(response.body, symbolize_names: true)
+    expect(resp).to eq("ok")
+  end
+end

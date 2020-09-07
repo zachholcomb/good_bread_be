7e9c6e64f26e41130c3a05dac3e1108327aaf82c
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-07 11:24:17 -0600

    Sets default value of 0 to role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+  after_initialize :set_role
 
   has_one :subscription
-  validates :name, :address, presence: true
+  validates :name, :address, :role, presence: true
   validates :email, uniqueness: true, presence: true
   validates :password_digest, presence: true
+
+  def set_role
+    self.role ||= 0
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,24 @@ RSpec.describe User do
     it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:address) }
     it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:role) }
   end
 
   describe "relationships" do
     it { should have_one :subscription }
+  end
+
+  describe "role" do
+    it "should have a default role of 0" do
+      user_params = {
+        email: "zach@gmail.com",
+        name: "Zach H",
+        password: "1234",
+        address: "499 Humboldt. Pl." 
+      }
+      
+      user = User.create(user_params)
+      expect(user.role).to eq(0)
+    end
   end
 end

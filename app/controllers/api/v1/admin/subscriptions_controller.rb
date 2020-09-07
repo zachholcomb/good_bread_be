e5b39963947db7b490e0981c090c3794a5a1c033
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-07 13:32:54 -0600

    Adds admin subscriptions controller

--- app/controllers/api/v1/admin/subscriptions_controller.rb
+++ app/controllers/api/v1/admin/subscriptions_controller.rb
@@ -1,4 +1,6 @@
-class Api::V1::SubscriptionsController < ApplicationController
+class Api::V1::Admin::SubscriptionsController < Api::V1::Admin::BaseController
+  before_action :authorize_access_request!, :require_admin
+
   def index
     render json: SubscriptionSerializer.new(Subscription.all)
   end

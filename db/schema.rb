67c755f0ed37a9e1d1155ce8ef0ca1ad385c3555
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-07 15:03:59 -0600

    Generate migrations for orders and order items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_171109) do
+ActiveRecord::Schema.define(version: 2020_09_07_210243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,22 @@ ActiveRecord::Schema.define(version: 2020_09_07_171109) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "status"
+    t.string "delivery_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -57,6 +73,8 @@ ActiveRecord::Schema.define(version: 2020_09_07_171109) do
     t.integer "role"
   end
 
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "shipment_items", "items"
   add_foreign_key "shipment_items", "shipments"
   add_foreign_key "shipments", "subscriptions"

b2303e6b14e2277820be8b9288dadd6648044364
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-14 16:40:25 -0600

    Adds order spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -9,5 +9,6 @@ RSpec.describe Order do
   describe 'relationships' do
     it { should have_many(:order_items) }
     it { should have_many(:items).through(:order_items) }
+    it { should belong_to(:user) }
   end
 end

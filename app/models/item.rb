b275435780771a4330b2897f2b28f26261376a1e
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-25 13:48:04 -0600

    Adds enum for item status and default method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,14 @@
 class Item < ApplicationRecord
+  after_initialize :set_defaults
+
   has_many :shipment_items
   has_many :shipments, through: :shipment_items
   has_many :order_items
   has_many :orders, through: :order_items
   validates :name, :price, presence: true
+  enum status: %w(Active Inactive)
+
+  def set_defaults
+    self.status ||= 0
+  end
 end

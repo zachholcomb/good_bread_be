7081d0987699dd9a41e971c82004b4761e0073e9
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-25 13:48:47 -0600

    Adds enum to subscription and sets default value

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,12 @@
 class Subscription < ApplicationRecord
+  after_initialize :set_subscription_type
+  
   validates :delivery_day, :subscription_type, presence: true
   belongs_to :user
   has_many :shipments, dependent: :destroy
+  enum subscription_type: %w(Monthly Bi-Monthly Weekly)
+
+  def set_subscription_type
+    self.subscription_type ||= 0
+  end
 end

ad6435e1864e67f6d90988dcc402eead58a8fc71
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-03 16:15:12 -0600

    Adds image to ItemSerializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,8 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :price, :status, :image
+  attributes :name, :price, :status
+
+  attribute :image do |object|
+    object.get_image_url if object.image.attached?
+  end
 end

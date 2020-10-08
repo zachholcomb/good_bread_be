dd7e69448848a76f23d8055bdf42310fa642a616
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-08 12:18:34 -0600

    Adds item_type and description to item serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,6 +1,6 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :price, :status, :type, :description
+  attributes :name, :price, :status, :item_type, :description
 
   attribute :image do |object|
     object.get_image_url if object.image.attached?

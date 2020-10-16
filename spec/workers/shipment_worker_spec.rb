a5a9352bb7fbda3f458c59a0fe468a7aa71efb24
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-16 15:47:38 -0600

    Adds spec for shipment_worker

---
+++ spec/workers/shipment_worker_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+RSpec.describe ShipmentWorker, type: :worker do
+  before(:each) do
+    Sidekiq::Worker.clear_all
+
+    @user = User.create!(
+        email: 'john@example.com',
+        name: 'John Wick',
+        address: '900 Victoria St.',
+        password: '1234',
+        password_confirmation: '1234'
+      )
+    subscription_params = {
+        subscription_type: 0,
+        delivery_day: "Monday",
+        user: @user
+      }
+    @subscription = Subscription.create!(subscription_params)
+  end
+
+  it 'adds job to the queue' do
+    expect(ShipmentWorker.jobs.size).to eq(0)
+
+    ShipmentWorker.perform_async(@subscription)
+    expect(ShipmentWorker.jobs.size).to eq(1)
+  end
+end

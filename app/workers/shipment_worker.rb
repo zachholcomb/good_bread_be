bcdfdcc4f437329adc4ebca7bf78407da8f0caaf
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-20 17:55:23 -0600

    Adds shipment worker perform method

--- app/workers/shipment_worker.rb
+++ app/workers/shipment_worker.rb
@@ -2,6 +2,14 @@ class ShipmentWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    p User.all
+    five_days_later = 432000
+    delivery_date = Time.now + five_days_later
+    
+    User.users_with_subscriptions.each do |user|
+      user.subscription.shipments.create(
+        status: 0,
+        delivery_date: delivery_date.strftime('%m/%d/%y')
+      )
+    end
   end
 end

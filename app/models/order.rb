1c3d83c930d210a24a5e9df319d6e33d3aa4cd72
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-28 17:49:04 -0600

    Adds calc_total method to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  after_initialize :set_status
+  after_initialize :set_defaults
 
   validates :status, presence: true
   has_many :order_items
@@ -7,7 +7,13 @@ class Order < ApplicationRecord
   belongs_to :user
   enum status: %w(Pending Shipped Delivered)
 
-  def set_status
+  def set_defaults
     self.status ||= 0
+    self.delivery_date ||= Time.now.strftime('%m/%d/%y')
+  end
+
+  def calc_total
+    self.total = order_items.sum('price * quantity')
+    self.save
   end
 end

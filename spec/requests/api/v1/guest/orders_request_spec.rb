26c6884f67e0cba6d6b88adf919d248f2afcd695
Zach Holcomb <zachholcombmusic@gmail.com>
2020-11-20 15:15:47 -0700

    Update spec for guest order that matches what the front end sends

--- spec/requests/api/v1/guest/orders_request_spec.rb
+++ spec/requests/api/v1/guest/orders_request_spec.rb
@@ -11,21 +11,23 @@ RSpec.describe 'guest orders' do
   it 'guest can order items' do
     order_params = {
       "items": {@item.id => {quantity: 1}, @item2.id => {quantity: 1} },
-      "status": "Pending",
-      "delivery_date": "9/5/2020",
-      "name": "Zach",
-      "email": "zach@example.com",
-      "address": "594 House Ave.",
-      "password": "password",
-      "password_confirmation": "password",
-      "role": 'guest'
+      "userParams": {
+        "status": "Pending",
+        "name": "Zach",
+        "email": "zach@example.com",
+        "address": "594 House Ave.",
+        "password": "password",
+        "password_confirmation": "password",
+        "role": 'guest'
+      }
     }
     post "/api/v1/orders", params: order_params
     expect(response).to be_successful
     expect(response.status).to eq(201)
 
     order_response = JSON.parse(response.body, symbolize_names: true)
-    expect(order_response[:order][:data][:attributes][:delivery_date]).to eq("9/5/2020")
+    order = Order.last
+    expect(order_response[:order][:data][:attributes][:delivery_date]).to eq(order.delivery_date)
     expect(order_response[:order][:data][:relationships][:items][:data].length).to eq(2)
     expect(order_response[:user][:data][:attributes][:email]).to eq('zach@example.com')
 

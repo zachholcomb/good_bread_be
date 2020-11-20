05916d4dd311aa63e724083faa8328dc4acad789
Zach Holcomb <zachholcombmusic@gmail.com>
2020-11-20 15:07:24 -0700

    Updates guest orders controller

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::OrdersController < ApplicationController
   def create
-    user = User.new(guest_params)
+    user = GuestCreator.create(params[:userParams])
     if user.save
       order = user.orders.create(order_params)
       params[:items].each do |(item, quantity)|
@@ -13,10 +13,6 @@ class Api::V1::OrdersController < ApplicationController
   end
 
   private
-
-  def guest_params
-    params.permit(:name, :email, :address, :password, :password_confirmation, :role)
-  end
 
   def order_params
     params.permit(:status, :delivery_date)
